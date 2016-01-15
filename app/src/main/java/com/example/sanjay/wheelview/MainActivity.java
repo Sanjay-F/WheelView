0b62ed7c1492c13e35dbc8b4e91f8904c75e58e6
Sanjay_f <406706576@qq.com>
2016-01-16 01:21:38 +0800

    +添加推荐库里面的一个选择插件

--- app/src/main/java/com/example/sanjay/wheelview/MainActivity.java
+++ app/src/main/java/com/example/sanjay/wheelview/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.sanjay.wheelview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.sanjay.wheelviewlib.RecycleWheelView;
 import com.example.sanjay.wheelviewlib.WheelAdapter;
+import com.example.sanjay.wheelviewlib.picker.DatePicker;
+import com.example.sanjay.wheelviewlib.picker.NumberPicker;
+import com.example.sanjay.wheelviewlib.picker.OptionPicker;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +51,44 @@ public class MainActivity extends AppCompatActivity {
 //        mAdapter.setTextPadding();
         dateWheel.setAdapter(mAdapter);
         dateWheel.setSelectedItem(50);
+
+    }
+
+    public void onDateClick(View view) {
+        final NumberPicker picker = new NumberPicker(this);
+        picker.setTopLineVisible(true);
+        picker.setTextColor(R.color.mainBlue);
+        picker.setLineColor(R.color.mainBlue);
+        picker.setTopLineColor(0XFFFFFF);
+        picker.setOptLineVisible(true);
+        picker.setCancelVisible(false);
+
+
+        picker.setOffset(2);
+        picker.setRange(145, 200);
+        picker.setSelectedItem(172);
+        picker.setLabel("厘米");
+        picker.setOnOptionPickListener(new OptionPicker.OnOptionPickListener() {
+            @Override
+            public void onOptionPicked(String option) {
+                Log.e("this", " value=" + option);
+                Log.e("sele", "value= " + picker.getSelectedValue());
+            }
+        });
+        picker.show();
+    }
+
+    public void onYearClick(View view) {
+        DatePicker picker = new DatePicker(this);
+        picker.setRange(2000, 2016);
+        picker.setSelectedItem(2015, 10, 10);
+        picker.setOnDatePickListener(new DatePicker.OnYearMonthDayPickListener() {
+            @Override
+            public void onDatePicked(String year, String month, String day) {
+                Log.e("this", year + "-" + month + "-" + day);
+            }
+        });
+        picker.show();
 
     }
 }
